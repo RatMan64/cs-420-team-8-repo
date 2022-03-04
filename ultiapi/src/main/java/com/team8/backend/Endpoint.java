a65625da8e87eef115dc5a19b06f356535e66e36
Toanuvo <Kzeezavadlov@yahoo.com>
2022-03-04 14:56:45 -0800

    setting up backend callbacks

--- ultiapi/src/main/java/com/team8/backend/Endpoint.java
+++ ultiapi/src/main/java/com/team8/backend/Endpoint.java
@@ -97,18 +97,5 @@ public class Endpoint {
 
         return "works";
     }
-    /**
-     * this is what needs to happen in this upadate api call
-     * 1. Either the partition key for the order needs to ge fished out of the json that siteflow gives
-     * or you use the order id that siteflow gives (this will need to be ironed out on how orderID is created for siteflow
-     * 2. update the item that corisponds to that id or uuid (specifically the status of the order)
-     **/
-    @PostMapping("/update")
-    public void update(@RequestBody JSONObject order_to_update)throws  NoSuchAlgorithmException, IOException, InvalidKeyException{
-        //todo grab order id from site flow response json  and update that orders status
-
-
-
-    }
 
 }
