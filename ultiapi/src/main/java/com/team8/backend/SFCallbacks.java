a657ec6eaaeb3bd0bc9e7a2d7c93537fe3bafc16
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2022-03-08 22:58:39 -0800

    Riley: todo added for order update. need to figure out if old order needs to be destroyed and redone

--- ultiapi/src/main/java/com/team8/backend/SFCallbacks.java
+++ ultiapi/src/main/java/com/team8/backend/SFCallbacks.java
@@ -40,10 +40,18 @@ public class SFCallbacks {
 
 
   @PostMapping("/ShipmentShipped")
-  public void ShipShipped(@RequestBody JSONObject shipped_order)throws NoSuchAlgorithmException, IOException, InvalidKeyException {
-
-
-
+  public void ShipShipped(@RequestBody String o)throws NoSuchAlgorithmException, IOException, InvalidKeyException {
+    // TODO: 3/8/2022 figureout what needs to be done interms of shipping information from siteflow
+      var order = new JSONObject(o);
+      String Customer = order.getString("customer");
+      String ID = order.getString("orderId");
+      String status = order.getString("status");
+      String tracUrl = order.getString("trackingNUM");
+      String trackNum = order.getString("trackingURL");
+      System.out.println("updating order for: " + Customer + " id: " + ID + " to status: " + status + " traking url: "+ tracUrl + " tracking Number: " + trackNum);
+      var ordershippupdate = DB.getItem(Key.builder().partitionValue(Customer).sortValue(ID).build());
+      //todo figureout if a new entry to the db is needed
+//      return ResponseEntity.ok().build();
   }
 
 
